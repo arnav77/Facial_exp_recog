3178d33545b012c13e6d4b520c8931fd9d4589df
Arnav Mathur <45800902+arnav77@users.noreply.github.com>
2020-09-27 16:57:40 +0530

    for verification

--- expression.py
+++ expression.py
@@ -1,1 +1,18 @@
 #psuedo code for recognition model
+import keras
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten, BatchNormalization
+from keras.layers import Conv2D, MaxPooling2D
+import os
+
+num_classes = 7
+img_rows, img_cols = 48,48
+batch_size = 8
+
+train_data = 'C:\Users\HP\Downloads\Facial exp recog\train'
+validation_data = 'C:\Users\HP\Downloads\Facial exp recog\validation'
+
+train_data_gen= ImageDataGenerator(rescale=1./255, rotation=30, shear_range=0.3, 
+
+)
